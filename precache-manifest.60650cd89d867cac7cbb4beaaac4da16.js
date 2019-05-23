359567cece45a2140bc54a0dfec2440b19db7045
Lisbel Cruz <Lcruztaveras@gmail.com>
2019-05-23 15:17:20 -0500

    Updates

---
+++ precache-manifest.60650cd89d867cac7cbb4beaaac4da16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c8dd17ca3a529ad099a5a9f4a38ee60",
+    "url": "/robot-friends/index.html"
+  },
+  {
+    "revision": "f5ab90645a425bff69ff",
+    "url": "/robot-friends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "6ed7edd53d10ca6c829c",
+    "url": "/robot-friends/static/css/main.09253676.chunk.css"
+  },
+  {
+    "revision": "f5ab90645a425bff69ff",
+    "url": "/robot-friends/static/js/2.f3d0f299.chunk.js"
+  },
+  {
+    "revision": "6ed7edd53d10ca6c829c",
+    "url": "/robot-friends/static/js/main.df86ce01.chunk.js"
+  },
+  {
+    "revision": "f8c63846f1fb9c939854",
+    "url": "/robot-friends/static/js/runtime~main.88802e26.js"
+  },
+  {
+    "revision": "5600967edd3f1a027665ba2572f23016",
+    "url": "/robot-friends/static/media/SEGA.5600967e.woff"
+  }
+]);
