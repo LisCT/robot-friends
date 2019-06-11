0c33c60c86bcef78f16123fdf472d3123c9d4df8
Lisbel Cruz <Lcruztaveras@gmail.com>
2019-06-11 16:49:53 -0500

    testing prettier

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,10 +5,11 @@ const Card = ({id, name, email}) => (
     <div className="tc bg-light-green dib br3 pa3 ma2 grow">
         <img src={`https://robohash.org/${id}?size=200x200`}alt="robot"/>
         <div>
-            <h2>{name}</h2>
+ <h2>{name}</h2>
+
             <p>{email}</p>
         </div>
-    </div>
+</div>
     
 );
 
